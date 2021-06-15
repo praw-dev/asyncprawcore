praw-dev/prawcore

Add two factor callback

tests/test_authorizer.py
@@ -5,7 +5,7 @@ from betamax import Betamax
 
 import prawcore
 
-from .conftest import (
+from .conftest import (  # noqa F401
     CLIENT_ID,
     CLIENT_SECRET,
     PASSWORD,
@@ -14,6 +14,7 @@ from .conftest import (
     REFRESH_TOKEN,
     REQUESTOR,
     TEMPORARY_GRANT_CODE,
+    two_factor_callback,
     USERNAME,
 )
 
@@ -339,6 +340,40 @@ class ScriptAuthorizerTest(AuthorizerTestBase):
         self.assertIsNotNone(authorizer.access_token)
         self.assertEqual(set(["*"]), authorizer.scopes)
         self.assertTrue(authorizer.is_valid())
+
+    def test_refresh_with__valid_otp(self):
+        authorizer = prawcore.ScriptAuthorizer(
+            self.authentication,
+            USERNAME,
+            PASSWORD,
+            lambda: "000000",
+        )
+        self.assertIsNone(authorizer.access_token)
+        self.assertIsNone(authorizer.scopes)
+        self.assertFalse(authorizer.is_valid())
+
+        with Betamax(REQUESTOR).use_cassette(
+            "ScriptAuthorizer_refresh_with__valid_otp"
+        ):
+            authorizer.refresh()
+
+        self.assertIsNotNone(authorizer.access_token)
+        self.assertEqual(set(["*"]), authorizer.scopes)
+        self.assertTrue(authorizer.is_valid())
+
+    def test_refresh_with__invalid_otp(self):
+        authorizer = prawcore.ScriptAuthorizer(
+            self.authentication,
+            USERNAME,
+            PASSWORD,
+            lambda: "fake",
+        )
+
+        with Betamax(REQUESTOR).use_cassette(
+            "ScriptAuthorizer_refresh_with__invalid_otp"
+        ):
+            self.assertRaises(prawcore.OAuthException, authorizer.refresh)
+            self.assertFalse(authorizer.is_valid())
 
     def test_refresh__with_invalid_username_or_password(self):
         authorizer = prawcore.ScriptAuthorizer(

tests/test_sessions.py
@@ -22,6 +22,7 @@ from .conftest import (
     REFRESH_TOKEN,
     REQUESTOR,
     USERNAME,
+    two_factor_callback,
 )
 
 
@@ -60,7 +61,9 @@ def script_authorizer():
     authenticator = prawcore.TrustedAuthenticator(
         REQUESTOR, CLIENT_ID, CLIENT_SECRET
     )
-    authorizer = prawcore.ScriptAuthorizer(authenticator, USERNAME, PASSWORD)
+    authorizer = prawcore.ScriptAuthorizer(
+        authenticator, USERNAME, PASSWORD, two_factor_callback
+    )
     authorizer.refresh()
     return authorizer
 

praw-dev/asyncprawcore

(cherry picked from commit 535715c743af8940b03a19fb03f4b54977ee379c)

tests/test_authorizer.py
@@ -4,7 +4,7 @@ import asynctest
 import asyncprawcore
 from asyncprawcore.requestor import Requestor
 
-from .conftest import (
+from .conftest import (  # noqa F401
     CLIENT_ID,
     CLIENT_SECRET,
     PASSWORD,
@@ -12,6 +12,7 @@ from .conftest import (
     REDIRECT_URI,
     REFRESH_TOKEN,
     TEMPORARY_GRANT_CODE,
+    two_factor_callback,
     USERNAME,
     VCR,
 )
@@ -359,6 +360,55 @@ class ScriptAuthorizerTest(AuthorizerTestBase):
         self.assertIsNotNone(authorizer.access_token)
         self.assertEqual({"*"}, authorizer.scopes)
         self.assertTrue(authorizer.is_valid())
+
+    async def test_refresh_with__valid_otp(self):
+        authorizer = asyncprawcore.ScriptAuthorizer(
+            self.authentication, USERNAME, PASSWORD, lambda: "000000"
+        )
+        self.assertIsNone(authorizer.access_token)
+        self.assertIsNone(authorizer.scopes)
+        self.assertFalse(authorizer.is_valid())
+
+        with VCR.use_cassette("ScriptAuthorizer_refresh_with__valid_otp"):
+            await authorizer.refresh()
+
+        self.assertIsNotNone(authorizer.access_token)
+        self.assertEqual(set(["*"]), authorizer.scopes)
+        self.assertTrue(authorizer.is_valid())
+
+    async def test_refresh_with__valid_otp_async(self):
+        async def code():
+            return "000000"
+
+        authorizer = asyncprawcore.ScriptAuthorizer(
+            self.authentication,
+            USERNAME,
+            PASSWORD,
+            code,
+        )
+        self.assertIsNone(authorizer.access_token)
+        self.assertIsNone(authorizer.scopes)
+        self.assertFalse(authorizer.is_valid())
+
+        with VCR.use_cassette("ScriptAuthorizer_refresh_with__valid_otp"):
+            await authorizer.refresh()
+
+        self.assertIsNotNone(authorizer.access_token)
+        self.assertEqual(set(["*"]), authorizer.scopes)
+        self.assertTrue(authorizer.is_valid())
+
+    async def test_refresh_with__invalid_otp(self):
+        authorizer = asyncprawcore.ScriptAuthorizer(
+            self.authentication,
+            USERNAME,
+            PASSWORD,
+            lambda: "fake",
+        )
+
+        with VCR.use_cassette("ScriptAuthorizer_refresh_with__invalid_otp"):
+            with self.assertRaises(asyncprawcore.OAuthException):
+                await authorizer.refresh()
+            self.assertFalse(authorizer.is_valid())
 
     async def test_refresh__with_invalid_username_or_password(self):
         authorizer = asyncprawcore.ScriptAuthorizer(

tests/test_sessions.py
@@ -19,6 +19,7 @@ from .conftest import (
     USERNAME,
     VCR,
     AsyncMock,
+    two_factor_callback,
 )
 
 
@@ -69,7 +70,9 @@ async def script_authorizer():
     authenticator = asyncprawcore.TrustedAuthenticator(
         requestor, CLIENT_ID, CLIENT_SECRET
     )
-    authorizer = asyncprawcore.ScriptAuthorizer(authenticator, USERNAME, PASSWORD)
+    authorizer = asyncprawcore.ScriptAuthorizer(
+        authenticator, USERNAME, PASSWORD, two_factor_callback
+    )
     await authorizer.refresh()
     return authorizer
 