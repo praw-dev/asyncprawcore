28e0bbda088e5da7b8f08e34047513618bb3b3c5
DevilXD <bartosz.peszek@gmail.com>
2022-02-22 21:56:31 +0100

    Implement delayed session creation

--- asyncprawcore/requestor.py
+++ asyncprawcore/requestor.py
@@ -1,6 +1,4 @@
 """Provides the HTTP request handling interface."""
-import asyncio
-
 import aiohttp
 
 from .const import TIMEOUT, __version__
@@ -11,10 +9,10 @@ class Requestor(object):
     """Requestor provides an interface to HTTP requests."""
 
     def __getattr__(self, attribute):  # pragma: no cover
-        """Pass all undefined attributes to the _http attribute."""
+        """Pass all undefined attributes to the _session attribute."""
         if attribute.startswith("__"):
-            raise AttributeError
-        return getattr(self._http, attribute)
+            raise AttributeError(attribute)
+        return getattr(self._session, attribute)
 
     def __init__(
         self,
@@ -22,7 +20,6 @@ class Requestor(object):
         oauth_url="https://oauth.reddit.com",
         reddit_url="https://www.reddit.com",
         session=None,
-        loop=None,
         timeout=TIMEOUT,
     ):
         """Create an instance of the Requestor class.
@@ -41,28 +38,31 @@ class Requestor(object):
         """
         if user_agent is None or len(user_agent) < 7:
             raise InvalidInvocation("user_agent is not descriptive")
-
-        self.loop = loop or asyncio.get_event_loop()
-        self._http = session or aiohttp.ClientSession(
-            loop=self.loop, timeout=aiohttp.ClientTimeout(total=None)
-        )
-        self._http._default_headers[
-            "User-Agent"
-        ] = f"{user_agent} asyncprawcore/{__version__}"
-
+        self._session = session
+        self._headers = {"User-Agent": f"{user_agent} asyncprawcore/{__version__}"}
         self.oauth_url = oauth_url
         self.reddit_url = reddit_url
         self.timeout = timeout
 
-    async def close(self):
+    def close(self):
         """Call close on the underlying session."""
-        return await self._http.close()
+        # NOTE: this is a pass-through coroutine
+        return self._session.close()
 
-    async def request(self, *args, timeout=None, **kwargs):
+    async def request(self, *args, **kwargs):
         """Issue the HTTP request capturing any errors that may occur."""
-        try:
-            return await self._http.request(
-                *args, timeout=timeout or self.timeout, **kwargs
+        if self._session is None:
+            self._session = aiohttp.ClientSession(
+                headers=self._headers,
+                timeout=aiohttp.ClientTimeout(total=None),
             )
+        if "timeout" not in kwargs:
+            kwargs["timeout"] = self.timeout
+        try:
+            # NOTE: Due to not using a context manager for the request-response cycle,
+            # calling code is expected to call the 'release' method on the returned
+            # response object once it's done with it.
+            # Reference 'aiohttp.ClientResponse.__aexit__' doing that.
+            return await self._session.request(*args, **kwargs)
         except Exception as exc:
             raise RequestException(exc, args, kwargs)
