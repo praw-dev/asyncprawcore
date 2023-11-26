praw-dev/prawcore

Add default instead of calculating window size (#147)

tests/integration/test_sessions.py
@@ -46,11 +46,15 @@ class TestSession(IntegrationTest):
         caplog.set_level(logging.DEBUG)
         session = prawcore.Session(script_authorizer)
         session.request("POST", "api/read_all_messages")
-        assert (
-            "prawcore",
-            logging.DEBUG,
-            "Response: 202 (2 bytes)",
-        ) in caplog.record_tuples
+        found_message = False
+        for package, level, message in caplog.record_tuples:
+            if (
+                package == "prawcore"
+                and level == logging.DEBUG
+                and "Response: 202 (2 bytes)" in message
+            ):
+                found_message = True
+        assert found_message, f"'Response: 202 (2 bytes)' in {caplog.record_tuples}"
 
     def test_request__bad_gateway(self, readonly_authorizer):
         session = prawcore.Session(readonly_authorizer)

praw-dev/asyncprawcore

(cherry picked from commit praw-dev/prawcore@e2a22e0fb28f5138461807642842fb5ff1fd5a94)

tests/integration/test_sessions.py
@@ -35,11 +35,15 @@ class TestSession(IntegrationTest):
         caplog.set_level(logging.DEBUG)
         session = asyncprawcore.Session(script_authorizer)
         await session.request("POST", "api/read_all_messages")
-        assert (
-            "asyncprawcore",
-            logging.DEBUG,
-            "Response: 202 (2 bytes)",
-        ) in caplog.record_tuples
+        found_message = False
+        for package, level, message in caplog.record_tuples:
+            if (
+                package == "asyncprawcore"
+                and level == logging.DEBUG
+                and "Response: 202 (2 bytes)" in message
+            ):
+                found_message = True
+        assert found_message, f"'Response: 202 (2 bytes)' in {caplog.record_tuples}"
 
     async def test_request__bad_gateway(
         self, readonly_authorizer: asyncprawcore.ReadOnlyAuthorizer