praw-dev/prawcore

Replace uses of time.time with time.monotonic(_ns)

prawcore/rate_limit.py
@@ -11,6 +11,8 @@ if TYPE_CHECKING:
 
     from requests.models import Response
 
+from prawcore.const import NANOSECONDS
+
 log = logging.getLogger(__package__)
 
 
@@ -20,8 +22,6 @@ class RateLimiter:
     Rate limits are controlled based on feedback from requests to Reddit.
 
     """
-
-    NANOSECONDS = 1_000_000_000
 
     def __init__(self, *, window_size: int):
         """Create an instance of the RateLimit class."""
@@ -57,8 +57,7 @@ class RateLimiter:
         if self.next_request_timestamp_ns is None:
             return
         sleep_seconds = (
-            float(self.next_request_timestamp_ns - time.monotonic_ns())
-            / self.NANOSECONDS
+            float(self.next_request_timestamp_ns - time.monotonic_ns()) / NANOSECONDS
         )
         if sleep_seconds <= 0:
             return
@@ -90,7 +89,7 @@ class RateLimiter:
 
         if self.remaining <= 0:
             self.next_request_timestamp_ns = now_ns + max(
-                self.NANOSECONDS / 2, seconds_to_reset * self.NANOSECONDS
+                NANOSECONDS / 2, seconds_to_reset * NANOSECONDS
             )
             return
 
@@ -110,5 +109,5 @@ class RateLimiter:
                 ),
                 10,
             )
-            * self.NANOSECONDS
+            * NANOSECONDS
         )

tests/unit/test_rate_limit.py
@@ -5,6 +5,7 @@ from unittest.mock import patch
 
 import pytest
 
+from prawcore.const import NANOSECONDS
 from prawcore.rate_limit import RateLimiter
 
 from . import UnitTest
@@ -14,7 +15,7 @@ class TestRateLimiter(UnitTest):
     @pytest.fixture
     def rate_limiter(self):
         rate_limiter = RateLimiter(window_size=600)
-        rate_limiter.next_request_timestamp_ns = 100 * RateLimiter.NANOSECONDS
+        rate_limiter.next_request_timestamp_ns = 100 * NANOSECONDS
         return rate_limiter
 
     @staticmethod
@@ -28,7 +29,7 @@ class TestRateLimiter(UnitTest):
     @patch("time.monotonic_ns")
     @patch("time.sleep")
     def test_delay(self, mock_sleep, mock_monotonic_ns, rate_limiter):
-        mock_monotonic_ns.return_value = 1 * RateLimiter.NANOSECONDS
+        mock_monotonic_ns.return_value = 1 * NANOSECONDS
         rate_limiter.delay()
         assert mock_monotonic_ns.called
         mock_sleep.assert_called_with(99)
@@ -38,7 +39,7 @@ class TestRateLimiter(UnitTest):
     def test_delay__no_sleep_when_time_in_past(
         self, mock_sleep, mock_monotonic_ns, rate_limiter
     ):
-        mock_monotonic_ns.return_value = 101 * RateLimiter.NANOSECONDS
+        mock_monotonic_ns.return_value = 101 * NANOSECONDS
         rate_limiter.delay()
         assert mock_monotonic_ns.called
         assert not mock_sleep.called
@@ -54,7 +55,7 @@ class TestRateLimiter(UnitTest):
     def test_delay__no_sleep_when_times_match(
         self, mock_sleep, mock_monotonic_ns, rate_limiter
     ):
-        mock_monotonic_ns.return_value = 100 * RateLimiter.NANOSECONDS
+        mock_monotonic_ns.return_value = 100 * NANOSECONDS
         rate_limiter.delay()
         assert mock_monotonic_ns.called
         assert not mock_sleep.called
@@ -63,22 +64,22 @@ class TestRateLimiter(UnitTest):
     def test_update__compute_delay_with_no_previous_info(
         self, mock_monotonic_ns, rate_limiter
     ):
-        mock_monotonic_ns.return_value = 100 * RateLimiter.NANOSECONDS
+        mock_monotonic_ns.return_value = 100 * NANOSECONDS
         rate_limiter.update(self._headers(60, 100, 60))
         assert rate_limiter.remaining == 60
         assert rate_limiter.used == 100
-        assert rate_limiter.next_request_timestamp_ns == 100 * RateLimiter.NANOSECONDS
+        assert rate_limiter.next_request_timestamp_ns == 100 * NANOSECONDS
 
     @patch("time.monotonic_ns")
     def test_update__compute_delay_with_single_client(
         self, mock_monotonic_ns, rate_limiter
     ):
         rate_limiter.window_size = 150
-        mock_monotonic_ns.return_value = 100 * RateLimiter.NANOSECONDS
+        mock_monotonic_ns.return_value = 100 * NANOSECONDS
         rate_limiter.update(self._headers(50, 100, 60))
         assert rate_limiter.remaining == 50
         assert rate_limiter.used == 100
-        assert rate_limiter.next_request_timestamp_ns == 110 * RateLimiter.NANOSECONDS
+        assert rate_limiter.next_request_timestamp_ns == 110 * NANOSECONDS
 
     @patch("time.monotonic_ns")
     def test_update__compute_delay_with_six_clients(
@@ -86,41 +87,41 @@ class TestRateLimiter(UnitTest):
     ):
         rate_limiter.remaining = 66
         rate_limiter.window_size = 180
-        mock_monotonic_ns.return_value = 100 * RateLimiter.NANOSECONDS
+        mock_monotonic_ns.return_value = 100 * NANOSECONDS
         rate_limiter.update(self._headers(60, 100, 72))
         assert rate_limiter.remaining == 60
         assert rate_limiter.used == 100
-        assert rate_limiter.next_request_timestamp_ns == 104.5 * RateLimiter.NANOSECONDS
+        assert rate_limiter.next_request_timestamp_ns == 104.5 * NANOSECONDS
 
     @patch("time.monotonic_ns")
     def test_update__delay_full_time_with_negative_remaining(
         self, mock_monotonic_ns, rate_limiter
     ):
-        mock_monotonic_ns.return_value = 37 * RateLimiter.NANOSECONDS
+        mock_monotonic_ns.return_value = 37 * NANOSECONDS
         rate_limiter.update(self._headers(0, 100, 13))
         assert rate_limiter.remaining == 0
         assert rate_limiter.used == 100
-        assert rate_limiter.next_request_timestamp_ns == 50 * RateLimiter.NANOSECONDS
+        assert rate_limiter.next_request_timestamp_ns == 50 * NANOSECONDS
 
     @patch("time.monotonic_ns")
     def test_update__delay_full_time_with_zero_remaining(
         self, mock_monotonic_ns, rate_limiter
     ):
-        mock_monotonic_ns.return_value = 37 * RateLimiter.NANOSECONDS
+        mock_monotonic_ns.return_value = 37 * NANOSECONDS
         rate_limiter.update(self._headers(0, 100, 13))
         assert rate_limiter.remaining == 0
         assert rate_limiter.used == 100
-        assert rate_limiter.next_request_timestamp_ns == 50 * RateLimiter.NANOSECONDS
+        assert rate_limiter.next_request_timestamp_ns == 50 * NANOSECONDS
 
     @patch("time.monotonic_ns")
     def test_update__delay_full_time_with_zero_remaining_and_no_sleep_time(
         self, mock_monotonic_ns, rate_limiter
     ):
-        mock_monotonic_ns.return_value = 37 * RateLimiter.NANOSECONDS
+        mock_monotonic_ns.return_value = 37 * NANOSECONDS
         rate_limiter.update(self._headers(0, 100, 0))
         assert rate_limiter.remaining == 0
         assert rate_limiter.used == 100
-        assert rate_limiter.next_request_timestamp_ns == 37.5 * RateLimiter.NANOSECONDS
+        assert rate_limiter.next_request_timestamp_ns == 37.5 * NANOSECONDS
 
     def test_update__no_change_without_headers(self, rate_limiter):
         prev = copy(rate_limiter)

praw-dev/asyncprawcore

(cherry picked from commit praw-dev/prawcore@ca482573caf48eca73ca5e08ad092c981bad0181)

asyncprawcore/rate_limit.py
@@ -13,6 +13,8 @@ if TYPE_CHECKING:
 
     from aiohttp import ClientResponse
 
+from asyncprawcore.const import NANOSECONDS
+
 log = logging.getLogger(__package__)
 
 
@@ -22,8 +24,6 @@ class RateLimiter:
     Rate limits are controlled based on feedback from requests to Reddit.
 
     """
-
-    NANOSECONDS = 1_000_000_000
 
     def __init__(self, *, window_size: int):
         """Create an instance of the RateLimit class."""
@@ -61,8 +61,7 @@ class RateLimiter:
         if self.next_request_timestamp_ns is None:
             return
         sleep_seconds = (
-            float(self.next_request_timestamp_ns - time.monotonic_ns())
-            / self.NANOSECONDS
+            float(self.next_request_timestamp_ns - time.monotonic_ns()) / NANOSECONDS
         )
         if sleep_seconds <= 0:
             return
@@ -94,7 +93,7 @@ class RateLimiter:
 
         if self.remaining <= 0:
             self.next_request_timestamp_ns = now_ns + max(
-                self.NANOSECONDS / 2, seconds_to_reset * self.NANOSECONDS
+                NANOSECONDS / 2, seconds_to_reset * NANOSECONDS
             )
             return
 
@@ -114,5 +113,5 @@ class RateLimiter:
                 ),
                 10,
             )
-            * self.NANOSECONDS
+            * NANOSECONDS
         )

tests/unit/test_rate_limit.py
@@ -5,6 +5,7 @@ from unittest.mock import patch
 
 import pytest
 
+from asyncprawcore.const import NANOSECONDS
 from asyncprawcore.rate_limit import RateLimiter
 
 from . import UnitTest
@@ -14,7 +15,7 @@ class TestRateLimiter(UnitTest):
     @pytest.fixture
     def rate_limiter(self):
         rate_limiter = RateLimiter(window_size=600)
-        rate_limiter.next_request_timestamp_ns = 100 * RateLimiter.NANOSECONDS
+        rate_limiter.next_request_timestamp_ns = 100 * NANOSECONDS
         return rate_limiter
 
     @staticmethod
@@ -28,7 +29,7 @@ class TestRateLimiter(UnitTest):
     @patch("time.monotonic_ns")
     @patch("asyncio.sleep")
     async def test_delay(self, mock_sleep, mock_monotonic_ns, rate_limiter):
-        mock_monotonic_ns.return_value = 1 * RateLimiter.NANOSECONDS
+        mock_monotonic_ns.return_value = 1 * NANOSECONDS
         await rate_limiter.delay()
         assert mock_monotonic_ns.called
         mock_sleep.assert_called_with(99)
@@ -38,7 +39,7 @@ class TestRateLimiter(UnitTest):
     async def test_delay__no_sleep_when_time_in_past(
         self, mock_sleep, mock_monotonic_ns, rate_limiter
     ):
-        mock_monotonic_ns.return_value = 101 * RateLimiter.NANOSECONDS
+        mock_monotonic_ns.return_value = 101 * NANOSECONDS
         await rate_limiter.delay()
         assert mock_monotonic_ns.called
         assert not mock_sleep.called
@@ -54,7 +55,7 @@ class TestRateLimiter(UnitTest):
     async def test_delay__no_sleep_when_times_match(
         self, mock_sleep, mock_monotonic_ns, rate_limiter
     ):
-        mock_monotonic_ns.return_value = 100 * RateLimiter.NANOSECONDS
+        mock_monotonic_ns.return_value = 100 * NANOSECONDS
         await rate_limiter.delay()
         assert mock_monotonic_ns.called
         assert not mock_sleep.called
@@ -63,22 +64,22 @@ class TestRateLimiter(UnitTest):
     def test_update__compute_delay_with_no_previous_info(
         self, mock_monotonic_ns, rate_limiter
     ):
-        mock_monotonic_ns.return_value = 100 * RateLimiter.NANOSECONDS
+        mock_monotonic_ns.return_value = 100 * NANOSECONDS
         rate_limiter.update(self._headers(60, 100, 60))
         assert rate_limiter.remaining == 60
         assert rate_limiter.used == 100
-        assert rate_limiter.next_request_timestamp_ns == 100 * RateLimiter.NANOSECONDS
+        assert rate_limiter.next_request_timestamp_ns == 100 * NANOSECONDS
 
     @patch("time.monotonic_ns")
     def test_update__compute_delay_with_single_client(
         self, mock_monotonic_ns, rate_limiter
     ):
         rate_limiter.window_size = 150
-        mock_monotonic_ns.return_value = 100 * RateLimiter.NANOSECONDS
+        mock_monotonic_ns.return_value = 100 * NANOSECONDS
         rate_limiter.update(self._headers(50, 100, 60))
         assert rate_limiter.remaining == 50
         assert rate_limiter.used == 100
-        assert rate_limiter.next_request_timestamp_ns == 110 * RateLimiter.NANOSECONDS
+        assert rate_limiter.next_request_timestamp_ns == 110 * NANOSECONDS
 
     @patch("time.monotonic_ns")
     def test_update__compute_delay_with_six_clients(
@@ -86,41 +87,41 @@ class TestRateLimiter(UnitTest):
     ):
         rate_limiter.remaining = 66
         rate_limiter.window_size = 180
-        mock_monotonic_ns.return_value = 100 * RateLimiter.NANOSECONDS
+        mock_monotonic_ns.return_value = 100 * NANOSECONDS
         rate_limiter.update(self._headers(60, 100, 72))
         assert rate_limiter.remaining == 60
         assert rate_limiter.used == 100
-        assert rate_limiter.next_request_timestamp_ns == 104.5 * RateLimiter.NANOSECONDS
+        assert rate_limiter.next_request_timestamp_ns == 104.5 * NANOSECONDS
 
     @patch("time.monotonic_ns")
     def test_update__delay_full_time_with_negative_remaining(
         self, mock_monotonic_ns, rate_limiter
     ):
-        mock_monotonic_ns.return_value = 37 * RateLimiter.NANOSECONDS
+        mock_monotonic_ns.return_value = 37 * NANOSECONDS
         rate_limiter.update(self._headers(0, 100, 13))
         assert rate_limiter.remaining == 0
         assert rate_limiter.used == 100
-        assert rate_limiter.next_request_timestamp_ns == 50 * RateLimiter.NANOSECONDS
+        assert rate_limiter.next_request_timestamp_ns == 50 * NANOSECONDS
 
     @patch("time.monotonic_ns")
     def test_update__delay_full_time_with_zero_remaining(
         self, mock_monotonic_ns, rate_limiter
     ):
-        mock_monotonic_ns.return_value = 37 * RateLimiter.NANOSECONDS
+        mock_monotonic_ns.return_value = 37 * NANOSECONDS
         rate_limiter.update(self._headers(0, 100, 13))
         assert rate_limiter.remaining == 0
         assert rate_limiter.used == 100
-        assert rate_limiter.next_request_timestamp_ns == 50 * RateLimiter.NANOSECONDS
+        assert rate_limiter.next_request_timestamp_ns == 50 * NANOSECONDS
 
     @patch("time.monotonic_ns")
     def test_update__delay_full_time_with_zero_remaining_and_no_sleep_time(
         self, mock_monotonic_ns, rate_limiter
     ):
-        mock_monotonic_ns.return_value = 37 * RateLimiter.NANOSECONDS
+        mock_monotonic_ns.return_value = 37 * NANOSECONDS
         rate_limiter.update(self._headers(0, 100, 0))
         assert rate_limiter.remaining == 0
         assert rate_limiter.used == 100
-        assert rate_limiter.next_request_timestamp_ns == 37.5 * RateLimiter.NANOSECONDS
+        assert rate_limiter.next_request_timestamp_ns == 37.5 * NANOSECONDS
 
     def test_update__no_change_without_headers(self, rate_limiter):
         prev = copy(rate_limiter)