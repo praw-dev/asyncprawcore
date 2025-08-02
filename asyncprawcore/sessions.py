praw-dev/prawcore

Drop support for python 3.8 and add 3.13 support

prawcore/sessions.py
@@ -205,11 +205,8 @@ class Session:
             )
             return response, None
         except RequestException as exception:
-            if (
-                not retry_strategy_state.should_retry_on_failure()
-                or not isinstance(  # noqa: E501
-                    exception.original_exception, self.RETRY_EXCEPTIONS
-                )
+            if not retry_strategy_state.should_retry_on_failure() or not isinstance(  # noqa: E501
+                exception.original_exception, self.RETRY_EXCEPTIONS
             ):
                 raise
             return None, exception.original_exception
@@ -266,9 +263,9 @@ class Session:
             raise self.STATUS_EXCEPTIONS[response.status_code](response)
         if response.status_code == codes["no_content"]:
             return None
-        assert (
-            response.status_code in self.SUCCESS_STATUSES
-        ), f"Unexpected status code: {response.status_code}"
+        assert response.status_code in self.SUCCESS_STATUSES, (
+            f"Unexpected status code: {response.status_code}"
+        )
         if response.headers.get("content-length") == "0":
             return ""
         try:

praw-dev/asyncprawcore

(cherry picked from commit praw-dev/prawcore@d78c1342200def5d21c87dea3e900d5e1bc9b544)

asyncprawcore/sessions.py
@@ -7,10 +7,10 @@ import logging
 import random
 import time
 from abc import ABC, abstractmethod
-from contextlib import asynccontextmanager
+from contextlib import AbstractAsyncContextManager, asynccontextmanager
 from copy import deepcopy
 from pprint import pformat
-from typing import TYPE_CHECKING, Any, AsyncContextManager, BinaryIO, Callable, TextIO
+from typing import TYPE_CHECKING, Any, BinaryIO, Callable, TextIO
 from urllib.parse import urljoin
 
 from aiohttp.web import HTTPRequestTimeout
@@ -191,7 +191,7 @@ class Session:
         timeout: float,
         url: str,
     ) -> Callable[
-        ..., AsyncContextManager[tuple[ClientResponse | None, Exception | None]]
+        ..., AbstractAsyncContextManager[tuple[ClientResponse | None, Exception | None]]
     ]:
         try:
             async with self._rate_limiter.call(
@@ -216,11 +216,8 @@ class Session:
                 )
                 yield response, None
         except RequestException as exception:
-            if (
-                not retry_strategy_state.should_retry_on_failure()
-                or not isinstance(  # noqa: E501
-                    exception.original_exception, self.RETRY_EXCEPTIONS
-                )
+            if not retry_strategy_state.should_retry_on_failure() or not isinstance(  # noqa: E501
+                exception.original_exception, self.RETRY_EXCEPTIONS
             ):
                 raise
             yield None, exception.original_exception
@@ -327,9 +324,9 @@ class Session:
                 raise self.STATUS_EXCEPTIONS[response.status](response)
             if response.status == codes["no_content"]:
                 return None
-            assert (
-                response.status in self.SUCCESS_STATUSES
-            ), f"Unexpected status code: {response.status}"
+            assert response.status in self.SUCCESS_STATUSES, (
+                f"Unexpected status code: {response.status}"
+            )
             if response.headers.get("content-length") == "0":
                 return ""
             try: